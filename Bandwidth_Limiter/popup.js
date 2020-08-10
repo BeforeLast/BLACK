f8bf58f6a5fa63ff6d8a8f86f0d729ee08bac135
epata <epatatuah180901@gmail.com>
2020-08-10 14:49:47 +0700

    ngapus timer.js sama ngedit popup html&js

--- Bandwidth_Limiter/popup.js
+++ Bandwidth_Limiter/popup.js
@@ -2,6 +2,31 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').addEventListener('click',
      onclick, false)
 
+    function limit () {
+        const options = {
+            createConnection(options) {
+                const socket = new net.Socket();
+                return socket.connect({host: options.host, port: options.port});
+            },
+            hostname: "212.183.159.230",
+            path: "/5MB.zip"
+        };
+        
+        const time = Date.now();
+        
+        const req = http.request(options, (res) => {
+        
+            res.pipe(new Throttle({rate: 200 * 1024}))
+                .on("data", (chunk) => {
+                    console.log(chunk.length);
+                })
+        
+            res.on("end", () => {
+                console.log("Done! Elapsed time: " + (Date.now() - time) + "ms");
+            });
+        });
+    }
+
     function onclick () {
         chrome.tabs.query({currentWindow: true, active: true},
             function (tabs) {
